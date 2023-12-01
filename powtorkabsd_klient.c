fdbe42b1a03168f51a943907f144f4756057aafe
Maksym Sierszen <yellowsoysauce@gmail.com>
2023-12-01 19:04:58 +0100

    up

--- powtorkabsd_klient.c
+++ powtorkabsd_klient.c
@@ -20,8 +20,8 @@ int main() {
 
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(4444);
-    //server_addr.sin_addr.s_addr = inet_addr("150.254.77.129"); // Adres serwera
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+    server_addr.sin_addr.s_addr = inet_addr("150.254.77.129"); // Adres serwera
+    //server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
 
     if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("Error connecting to server");
